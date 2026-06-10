cf0b8f59bcd597bfd98d8d91b43e208f41fcd927
oussama <sarbouteoussama.com>
2026-06-10 15:54:46 +0100

    not enough data

---
+++ cleaning_clustring/nb_colonnes.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("clean_comments.csv")
+
+#file = open("nb_colonne.txt", "w")
+
+
+print(df.columns)
--- cleaning_clustring/vect_clf.py
+++ cleaning_clustring/vect_clf.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("clean_comments.csv")
 
 # ── 1. Filtrer par médicament ──────────────────────────────────────────────────
-chemical = "sertraline"  # viendra du UI plus tard
+chemical = "fluoxetine"  # viendra du UI plus tard
 drug_df = df[df["chemical_name"] == chemical].copy()
 print(f"Commentaires pour {chemical} : {len(drug_df)}")
 
@@ -21,7 +21,7 @@ X = vectorizer.fit_transform(drug_df["clean_text"])
 
 # ── 3. Trouver le bon k avec la méthode du coude ──────────────────────────────
 inertias = []
-K_range = range(2, 10)
+K_range = range(2, 60)
 
 for k in K_range:
     km = KMeans(n_clusters=k, random_state=42, n_init=10)
@@ -32,15 +32,16 @@ for k in K_range:
 # Plot
 
 plt.scatter(K_range, inertias)
-plt.plot(K_range, inertias)
-
+plt.plot(K_range, inertias, )
+plt.xlabel("Valeur de K")
+plt.ylabel("WCSS")
 #plt.tight_layout()
 plt.savefig("optimal_k.png")
 plt.show()
 
 # ── 4. KMeans avec le k optimal ───────────────────────────────────────────────
 # Choisis k d'après le graphe — on suppose k=5 ici
-OPTIMAL_K = 8
+OPTIMAL_K = 46
 
 kmeans = KMeans(n_clusters=OPTIMAL_K, random_state=42, n_init=10)
 drug_df["cluster"] = kmeans.fit_predict(X)
@@ -79,5 +80,5 @@ print(profile[["theme", "nb_mentions"]])
 # ── 8. Sauvegarder ────────────────────────────────────────────────────────────
 drug_df.to_csv("comments_clustered.csv", index=False)
 
-#print(X.shape)
-#print(vectorizer.get_feature_names_out())
+#print(f"La taille de la matrice TF-IDF pour le sertraline : {X.shape}")
+#print(f"Les colonnes (mots) de la matrice : {vectorizer.get_feature_names_out()}")
